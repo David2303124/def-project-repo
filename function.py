612c80cadb358cf3a01b805be3561234b3770a28
Eremeev David <eredamdavmail.ru@gmail.com>
2026-01-06 22:40:36 +0200

    Add fails

---
+++ function.py
@@ -0,0 +1,8 @@
+def salut(nume):
+    return f"Salut, {nume}!"
+
+rezultat = salut("David")
+print(rezultat)
+
+print(salut("Ion"))
+
---
+++ function3.py
@@ -0,0 +1,12 @@
+def carte_de_vizita():
+    print("Andrei Muntean")
+    print("Avocat")
+    print("Nr. te: 060-123-456")
+
+carte_de_vizita()
+
+
+def spune_buna():
+    print("Buna ziua!")
+
+spune_buna()
